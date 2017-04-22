a56e06207d7d3ff0ad65ecb27b021e39c8faa48b
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-23 03:06:38 +0900

    Update custom button class.

--- PhotoGurashi/View/CustomButton.swift
+++ PhotoGurashi/View/CustomButton.swift
@@ -14,10 +14,34 @@ class CustomButton: UIButton {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
-        self.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        self.layer.masksToBounds = true
-        self.titleLabel?.font = UIFont.systemFont(ofSize: UIFont.buttonFontSize)
-        self.layer.cornerRadius = 40.0
+        frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        backgroundColor = randomColor()
+        layer.masksToBounds = true
+        titleLabel?.font = UIFont.systemFont(ofSize: UIFont.buttonFontSize)
+        setTitleColor(UIColor.white, for: UIControlState())
+        layer.cornerRadius = 40.0
     }
 
+    func customAddTarget(vc: UIViewController) {
+        addTarget(vc, action: #selector(vc.onClickButton(_:)), for: .touchUpInside)
+    }
+
+    func customSetTitle(title: String) {
+        setTitle(title, for: UIControlState())
+    }
+
+    func randomColor() -> UIColor {
+        switch arc4random_uniform(5) {
+        case 0: return .magenta
+        case 1: return .purple
+        case 2: return .red
+        case 3: return .orange
+        case 4: return .yellow
+        default: return .clear
+        }
+    }
+}
+
+extension UIViewController {
+    func onClickButton(_ sender: UIButton) { }
 }
