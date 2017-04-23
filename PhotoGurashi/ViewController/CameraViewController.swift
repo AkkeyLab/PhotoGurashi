8837f0bc1100962c2fa0c4770b5ccb74e9e33162
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-23 20:20:04 +0900

    Camera VC refactoring.

--- PhotoGurashi/ViewController/CameraViewController.swift
+++ PhotoGurashi/ViewController/CameraViewController.swift
@@ -10,210 +10,192 @@ import UIKit
 import CoreImage
 import AVFoundation
 
+enum CameraDirection {
+    case front
+    case back
+}
+
 class CameraViewController: UIViewController {
 
-    //***Field***
-    var mySession: AVCaptureSession!
-    var myDevice: AVCaptureDevice!
-    var myImageOutput: AVCaptureStillImageOutput!
-    let cameraButton: UIButton = UIButton()
-    let exitButton: UIButton = UIButton()
-    let changeCameraButton: UIButton = UIButton()
-    var provisionalImageView: UIImageView!
-    var provisionalImage: UIImage = UIImage(named: "stamp_yuki")!
+    private var screenHeight: CGFloat!
+    private var screenWidth: CGFloat!
+    private var charaSize: CGRect!
+    private var charaPosition: CGPoint!
 
-    var cameraInfo: Bool = true
-    //Take a picture
-    var takeInputImage: CIImage!
-    //****END****
+    // Character image view
+    private var charaImageView: UIImageView!
+    private var charaImage: UIImage!
 
-    //Initializer
-    func changeImage(_ image: UIImage) {
-        self.provisionalImage = image
-    }
+    private var cameraSession: AVCaptureSession!
+    private var cameraInfo: CameraDirection!
+    private var imageOutput: AVCaptureStillImageOutput!
 
-    /*
-    ***Session list***
-    
-    1->Caputure
-    2->VideoInput
-    3->VideoOutput
-    
-    4->Start
-    ******************
-    
-    ***Add View list ***
-    
-    1->myVideoLayer
-    2->provisionalImageView
-    3->CameraButton
-    ********************
-    */
+    private let cameraButton: UIButton = UIButton()
+    private let exitButton: UIButton = UIButton()
+    private let changeCameraButton: UIButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setValue()
+        setCamera()
+        setParts()
+    }
 
-        //Camera
-        cameraSetting()
+    func setValue() {
+        screenHeight = self.view.bounds.height
+        screenWidth = self.view.bounds.width
 
-        //Add provisional Image
-        provisionalImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: (self.view.bounds.width / 2) + (self.view.bounds.width / 4),
-                                                         height: (self.view.bounds.height / 2) + (self.view.bounds.height / 4)))
+        charaSize = CGRect(x: 0, y: 0, width: (screenWidth / 2) + (screenWidth / 4), height: (screenHeight / 2) + (screenHeight / 4))
+        charaPosition = CGPoint(x: (screenWidth / 2) + (screenWidth / 8), y: (screenHeight / 2) + (screenHeight / 8))
 
-        //memo: Move to field
-        //let provisionalImage = UIImage(named: "image/yuki.png")
-        provisionalImageView.image = provisionalImage
-        provisionalImageView.layer.position = CGPoint(x: (self.view.bounds.width / 2) + (self.view.bounds.width / 8),
-                                                      y: (self.view.bounds.height / 2) + (self.view.bounds.height / 8))
-        //Add View
-        self.view.addSubview(provisionalImageView)
+        charaImage = UIImage(named: "stamp_yuki") // Default
+    }
 
-        //ex) let cameraButton = UIButton(frame: CGRectMake(0,0,120,50))
-        cameraButton.frame = CGRect(x: 0, y: 0, width: 120, height: 50)//size
-        cameraButton.backgroundColor = UIColor.red;
+    func setParts() {
+        charaImageView = UIImageView(frame: charaSize)
+        charaImageView.layer.position = charaPosition
+        if let image = charaImage {
+            charaImageView.image = image
+        }
+        view.addSubview(charaImageView)
+
+        cameraButton.frame = CGRect(x: 0, y: 0, width: 120, height: 50)
+        cameraButton.backgroundColor = .red
         cameraButton.layer.masksToBounds = true
         cameraButton.setTitle("撮影", for: UIControlState())
         cameraButton.layer.cornerRadius = 20.0
-        cameraButton.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height - 50)
+        cameraButton.layer.position = CGPoint(x: screenWidth / 2, y: screenHeight - 50)
         cameraButton.addTarget(self, action: #selector(CameraViewController.onClickButton(_:)), for: .touchUpInside)
-        cameraButton.tag = 0//Add button tag
-        //Add View
-        self.view.addSubview(cameraButton)
+        cameraButton.tag = 0
+        view.addSubview(cameraButton)
 
-        //ex) let exitButton = UIButton(frame: CGRectMake(0,0,80,80))
-        exitButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)//size
-        exitButton.backgroundColor = UIColor.cyan
+        exitButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        exitButton.backgroundColor = .cyan
         exitButton.layer.masksToBounds = true
         exitButton.setTitle("戻る", for: UIControlState())
         exitButton.titleLabel?.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
         exitButton.setTitleColor(UIColor.darkGray, for: UIControlState())
-        exitButton.layer.cornerRadius = 10.0//edge
-        exitButton.layer.position = CGPoint(x: 50, y: self.view.bounds.height - 50)
+        exitButton.layer.cornerRadius = 10.0
+        exitButton.layer.position = CGPoint(x: 50, y: screenHeight - 50)
         exitButton.addTarget(self, action: #selector(CameraViewController.onClickButton(_:)), for: .touchUpInside)
         exitButton.tag = 1
-        self.view.addSubview(exitButton)
+        view.addSubview(exitButton)
 
-        //ex) let changeCameraButton = UIButton(frame: CGRectMake(0,0,80,80))
-        changeCameraButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)//size
-        changeCameraButton.backgroundColor = UIColor.gray
+        changeCameraButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        changeCameraButton.backgroundColor = .gray
         changeCameraButton.layer.masksToBounds = true
         changeCameraButton.setTitle("変更", for: UIControlState())
         changeCameraButton.titleLabel?.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
         changeCameraButton.setTitleColor(UIColor.white, for: UIControlState())
-        changeCameraButton.layer.cornerRadius = 25.0//edge
+        changeCameraButton.layer.cornerRadius = 25.0
         changeCameraButton.layer.position = CGPoint(x: self.view.bounds.width - 50, y: 50)
         changeCameraButton.addTarget(self, action: #selector(CameraViewController.onClickButton(_:)), for: .touchUpInside)
         changeCameraButton.tag = 2
-        self.view.addSubview(changeCameraButton)
+        view.addSubview(changeCameraButton)
     }
 
-    //Button event
-    override func onClickButton(_ sender: UIButton) {
+    func setCharaImage(_ name: String) {
+        if let image = UIImage(named: name) {
+            charaImage = image
+            charaImageView.image = charaImage
+        }
+    }
 
+    override func onClickButton(_ sender: UIButton) {
         switch sender.tag {
         case 0:
-            //Lets output image
-            outputImage()
+            outputPhoto()
         case 1:
-            //Lets exit
-            self.dismiss(animated: true, completion: nil)
+            dismiss(animated: true, completion: nil)
         case 2:
-            cameraInfo = !cameraInfo
-            viewDidLoad()
+            changeCameraDirection()
         default:
-            print("ERROR", terminator: "")
+            NSLog("Button tag error")
         }
-
     }
 
-    func cameraSetting() {
+    func setCamera() {
+        cameraInfo = .back
+        cameraSession = AVCaptureSession()
+        imageOutput = AVCaptureStillImageOutput()
+        cameraSession.addOutput(imageOutput)
 
-        mySession = AVCaptureSession()
+        do {
+            let videoInput: AVCaptureInput = try AVCaptureDeviceInput.init(device: setDevice(cameraInfo))
+            cameraSession.addInput(videoInput)
+        } catch {
+            NSLog("AVCaptureInput get error")
+        }
 
-        let devices = AVCaptureDevice.devices()//Add all devices
+        let videoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.init(session: cameraSession)
+        videoLayer.frame = self.view.bounds
+        videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        view.layer.addSublayer(videoLayer)
+
+        cameraSession.startRunning()
+    }
+
+    func changeCameraDirection() {
+        cameraSession.stopRunning()
+
+        if cameraInfo == .back {
+            cameraInfo = .front
+        } else {
+            cameraInfo = .back
+        }
+
+        do {
+            let videoInput: AVCaptureInput = try AVCaptureDeviceInput.init(device: setDevice(cameraInfo))
+            cameraSession.addInput(videoInput)
+        } catch {
+            NSLog("AVCaptureInput get error")
+        }
+
+        cameraSession.startRunning()
+    }
+
+    func setDevice(_ direction: CameraDirection) -> AVCaptureDevice? {
+        let devices = AVCaptureDevice.devices()
         for device in devices! {
-            if(cameraInfo) { //Select back camera
-                if((device as AnyObject).position == AVCaptureDevicePosition.back) {
-                    myDevice = device as! AVCaptureDevice
+            if cameraInfo == .back {
+                if (device as AnyObject).position == .back {
+                    return device as? AVCaptureDevice
                 }
-            } else { //Select front camera
-                if((device as AnyObject).position == AVCaptureDevicePosition.front) {
-                    myDevice = device as! AVCaptureDevice
+            } else {
+                if (device as AnyObject).position == .front {
+                    return device as? AVCaptureDevice
                 }
             }
-
         }
-        //let videoInput = (try! AVCaptureDeviceInput.deviceInputWithDevice(myDevice))
-        let videoInput: AVCaptureInput!
-        do {
-            videoInput = try AVCaptureDeviceInput.init(device: myDevice)
-        } catch {
-            videoInput = nil
-        }
-        mySession.addInput(videoInput)
-
-        myImageOutput = AVCaptureStillImageOutput()
-        mySession.addOutput(myImageOutput)
-
-        //let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as AVCaptureVideoPreviewLayer
-        let myVideoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.init(session: mySession)
-        myVideoLayer.frame = self.view.bounds
-        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        //Add View
-        self.view.layer.addSublayer(myVideoLayer)
-
-        mySession.startRunning()
+        return nil
     }
 
-    func outputImage() {
+    func outputPhoto() {
+        let videoConnection = imageOutput.connection(withMediaType: AVMediaTypeVideo)
+        imageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (imageDataBuffer, error) -> Void in
+            // Convert DataBuffer of the acquired Image to Jpeg.
+            let imageData: Data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+            let image: UIImage = UIImage(data: imageData)!
 
-        //It is connected to the video output
-        let myVideoConnection = myImageOutput.connection(withMediaType: AVMediaTypeVideo)
+            // Edit image
+            UIGraphicsBeginImageContext(CGSize(width: image.size.width, height: image.size.height))
+            image.draw(at: CGPoint(x: 0, y: 0))
 
-        //Get the image from the connection
-        self.myImageOutput.captureStillImageAsynchronously(from: myVideoConnection, completionHandler: { (imageDataBuffer, error) -> Void in
-        //Convert DataBuffer of the acquired Image to Jpeg.
-        let myImageData: Data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
-        let myImage: UIImage = UIImage(data: myImageData)!
+            let charaSizeX = (image.size.width / 2) + (image.size.width / 4)
+            let charaSizeY = (image.size.height / 2) + (image.size.height / 4)
+            let drawposition = CGRect(x: image.size.width - charaSizeX, y: image.size.height - charaSizeY, width: charaSizeX, height: charaSizeY)
+            if self.charaImage != nil {
+                self.charaImage.draw(in: drawposition)
+            }
 
-        //Image synthesis (memo: Move to field)
-        //let provisionalImage = UIImage(named: "image/yuki.png")
-        //Main size is take image (No conversion take-image-size)
-        UIGraphicsBeginImageContext(CGSize(width: myImage.size.width, height: myImage.size.height))
-        myImage.draw(at: CGPoint(x: 0, y: 0))
-        //Synthesis image
-        let provisionalSizeX = (myImage.size.width / 2) + (myImage.size.width / 4)
-        let provisionalSizeY = (myImage.size.height / 2) + (myImage.size.height / 4)
-        self.provisionalImage.draw(in: CGRect(x: myImage.size.width - provisionalSizeX,
-                                              y: myImage.size.height - provisionalSizeY,
-                                              width: provisionalSizeX,
-                                              height: provisionalSizeY))
-        //Remake image
-        let reMyImage = UIGraphicsGetImageFromCurrentImageContext()
-        //End synthesis
-        UIGraphicsEndImageContext()
+            let editImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            // END
 
-        //Add album
-        UIImageWriteToSavedPhotosAlbum(reMyImage!, self, nil, nil)
-
+            if editImage != nil {
+                //Add album
+                UIImageWriteToSavedPhotosAlbum(editImage!, self, nil, nil)
+            }
         })
-
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
